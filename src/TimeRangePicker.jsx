8e4187e30b7b13547b1f29ac4017e87c78a96c52
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-10-07 12:30:30 +0200

    Implement support for event props

--- src/TimeRangePicker.jsx
+++ src/TimeRangePicker.jsx
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { polyfill } from 'react-lifecycles-compat';
+import makeEventProps from 'make-event-props';
 import mergeClassNames from 'merge-class-names';
 import detectElementOverflow from 'detect-element-overflow';
 
@@ -25,6 +26,10 @@ export default class TimeRangePicker extends PureComponent {
   }
 
   state = {};
+
+  get eventProps() {
+    return makeEventProps(this.props);
+  }
 
   componentDidMount() {
     document.addEventListener('mousedown', this.onClick);
@@ -63,13 +68,18 @@ export default class TimeRangePicker extends PureComponent {
     this.onChange([valueFrom, valueTo], closeClock);
   }
 
-  onFocus = () => {
-    const { disabled } = this.props;
+  onFocus = (event) => {
+    const { disabled, onFocus } = this.props;
+
+    if (onFocus) {
+      onFocus(event);
+    }
 
     // Internet Explorer still fires onFocus on disabled elements
     if (disabled) {
       return;
     }
+
     this.openClock();
   }
 
@@ -237,6 +247,7 @@ export default class TimeRangePicker extends PureComponent {
           `${baseClassName}--${disabled ? 'disabled' : 'enabled'}`,
           className,
         )}
+        {...this.eventProps}
         onFocus={this.onFocus}
         ref={(ref) => { this.wrapper = ref; }}
       >
