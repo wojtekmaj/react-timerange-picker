2bf510d0a5516328067bb2ff3c4d01c82ec4205c
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-05-01 12:54:37 +0200

    Get rid of callIfDefined() in favor of a simple if()

--- src/TimeRangePicker.jsx
+++ src/TimeRangePicker.jsx
@@ -9,7 +9,6 @@ import Clock from 'react-clock/dist/entry.nostyle';
 import TimeInput from 'react-time-picker/dist/TimeInput';
 
 import { isTime } from './shared/propTypes';
-import { callIfDefined } from './shared/utils';
 
 const allViews = ['hour', 'minute', 'second'];
 const baseClassName = 'react-timerange-picker';
@@ -39,7 +38,8 @@ export default class TimeRangePicker extends PureComponent {
 
     if (isOpen !== prevState.isOpen) {
       this.handleOutsideActionListeners();
-      callIfDefined(isOpen ? onClockOpen : onClockClose);
+      const callback = isOpen ? onClockOpen : onClockClose;
+      if (callback) callback();
     }
   }
 
