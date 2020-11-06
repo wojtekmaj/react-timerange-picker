cc5bb61ecf97724549a570a76d3f2ac19d0fdc45
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-11-06 10:55:54 +0100

    Remove arrow-parens rule override

--- sample/Sample.jsx
+++ sample/Sample.jsx
@@ -12,7 +12,7 @@ export default class Sample extends Component {
     value: [now, nextHour],
   }
 
-  onChange = value => this.setState({ value })
+  onChange = (value) => this.setState({ value })
 
   render() {
     const { value } = this.state;
--- src/TimeRangePicker.jsx
+++ src/TimeRangePicker.jsx
@@ -112,10 +112,10 @@ export default class TimeRangePicker extends PureComponent {
   }
 
   toggleClock = () => {
-    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+    this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
   }
 
-  stopPropagation = event => event.stopPropagation();
+  stopPropagation = (event) => event.stopPropagation();
 
   clear = () => this.onChange(null);
 
@@ -124,7 +124,7 @@ export default class TimeRangePicker extends PureComponent {
 
     const shouldListenWithFallback = typeof shouldListen !== 'undefined' ? shouldListen : isOpen;
     const fnName = shouldListenWithFallback ? 'addEventListener' : 'removeEventListener';
-    outsideActionEvents.forEach(eventName => document[fnName](eventName, this.onOutsideAction));
+    outsideActionEvents.forEach((eventName) => document[fnName](eventName, this.onOutsideAction));
   }
 
   renderInputs() {
