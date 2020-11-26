c5b448f3275a523e119a1de42dc48eac4e1375e7
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-11-26 10:44:47 +0100

    Update sample

--- sample/Sample.jsx
+++ sample/Sample.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import TimeRangePicker from '@wojtekmaj/react-timerange-picker';
 
 import './Sample.less';
@@ -7,30 +7,22 @@ const now = new Date();
 const nextHour = new Date();
 nextHour.setHours(nextHour.getHours() + 1);
 
-export default class Sample extends Component {
-  state = {
-    value: [now, nextHour],
-  }
+export default function Sample() {
+  const [value, onChange] = useState([now, nextHour]);
 
-  onChange = (value) => this.setState({ value })
-
-  render() {
-    const { value } = this.state;
-
-    return (
-      <div className="Sample">
-        <header>
-          <h1>react-timerange-picker sample page</h1>
-        </header>
-        <div className="Sample__container">
-          <main className="Sample__container__content">
-            <TimeRangePicker
-              onChange={this.onChange}
-              value={value}
-            />
-          </main>
-        </div>
+  return (
+    <div className="Sample">
+      <header>
+        <h1>react-timerange-picker sample page</h1>
+      </header>
+      <div className="Sample__container">
+        <main className="Sample__container__content">
+          <TimeRangePicker
+            onChange={onChange}
+            value={value}
+          />
+        </main>
       </div>
-    );
-  }
+    </div>
+  );
 }
