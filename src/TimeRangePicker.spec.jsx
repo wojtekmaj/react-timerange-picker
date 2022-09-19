c7eb5664c8c0e72eaafbf4c92c397f322f73052d
Wojciech Maj <kontakt@wojtekmaj.pl>
2022-09-19 14:26:23 +0200

    Add unit test for applying --open className

--- src/TimeRangePicker.spec.jsx
+++ src/TimeRangePicker.spec.jsx
@@ -161,6 +161,14 @@ describe('TimeRangePicker', () => {
     expect(wrapper).toHaveClass(className);
   });
 
+  it('applies "--open" className to its wrapper when given isOpen flag', () => {
+    const { container } = render(<TimeRangePicker isOpen />);
+
+    const wrapper = container.firstChild;
+
+    expect(wrapper).toHaveClass('react-timerange-picker--open');
+  });
+
   it('applies clockClassName to the clock when given a string', () => {
     const clockClassName = 'testClassName';
 
