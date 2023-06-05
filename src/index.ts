0765d124b052f046b187f91fe7e5d5d22e185b9a
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-06-05 22:39:15 +0200

    Simplify component props type export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import TimeRangePicker from './TimeRangePicker';
 
-import type { TimeRangePickerProps } from './TimeRangePicker';
+export type { TimeRangePickerProps } from './TimeRangePicker';
 
 export { TimeRangePicker };
-
-export type { TimeRangePickerProps };
 
 export default TimeRangePicker;
